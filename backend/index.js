582eeed77be372f4aecbfc719722313bdf454e40
Hilary Titus Naor <hilarytn@gmail.com>
2023-10-17 22:11:52 +0100

    Introduce routes and controller directories

--- backend/index.js
+++ backend/index.js
@@ -11,34 +11,31 @@ const HOST = process.env.HOST;
 app.get("/", (req, res) => {
     res.send('Welcome to this site!');
     console.log('The Landing Page endpoint has been hit!')
-})
+});
+
 app.get("/api/:date?", (req, res) => {
     let dateToParse = req.params.date;
-    //let dateToInt = parseInt(dateToParse);
-    //let newDate = new Date(dateToInt);
-    let newDate = new Date(parseInt(dateToParse));
-    //let unixDate = dateToInt;
-    let unixDate = parseInt(dateToParse);
-
-    if(!dateToParse) {
-        let noDateUnix = new Date()
-        res.json({
-            unix:  Math.ceil(noDateUnix.getTime()/1000),
-            utc: noDateUnix.toUTCString()
-        })
-    }
-
-    if (newDate == "Invalid Date" ) {
-        res.send({error: newDate.toUTCString()})
-    }
-    let utcDate = newDate.toUTCString();
-    res.json(
-        {
-            unix: unixDate,
-            utc: utcDate
-        })
-})
-        //}
     
+    if (!dateToParse) {
+        let noDateUnix = new Date();
+        res.json({
+            unix:  Math.ceil(noDateUnix.getTime() / 1000),
+            utc: noDateUnix.toUTCString()
+        });
+    } else {
+        let newDate = new Date(parseInt(dateToParse));
+        let unixDate = parseInt(dateToParse);
 
- app.listen (PORT, HOST, () => console.log(`Server running on ${HOST}:${PORT}`))
+        if (newDate.toString() === "Invalid Date") {
+            res.json({ error: "Invalid Date" });
+        } else {
+            let utcDate = newDate.toUTCString();
+            res.json({
+                unix: unixDate,
+                utc: utcDate
+            });
+        }
+    }
+});
+
+app.listen(PORT, HOST, () => console.log(`Server running on ${HOST}:${PORT}`));
