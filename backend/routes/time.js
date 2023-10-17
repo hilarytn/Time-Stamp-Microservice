97fc583e2a0348b0c96e626423c8ef1a9c12aff6
Hilary Titus Naor <hilarytn@gmail.com>
2023-10-17 22:24:38 +0100

    apply router to the getDate ndpoint via teh controller

--- backend/routes/time.js
+++ backend/routes/time.js
@@ -1,5 +1,8 @@
 import express from 'express';
-
+import { getDate } from '../controllers/TimeController';
 
 const router = express.Router();
 
+router.get('/:date?', getDate);
+
+export default router;
