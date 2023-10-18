1a3e3bc9a632ab3d7ad6b115362cf77afaecf6a6
Hilary Titus Naor <hilarytn@gmail.com>
2023-10-18 13:58:44 +0100

    Read data from template render

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
 import router from "./routes/time.js";
-import path from 'path';
 
 dotenv.config({ path: '../.env' });
 
@@ -10,6 +9,7 @@ const app = express();
 const PORT = process.env.PORT;
 const HOST = process.env.HOST;
 
+app.use(express.static('public'));
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 app.use("/api", router)
@@ -17,7 +17,8 @@ app.use("/api", router)
 
 
 app.get("/", (req, res) => {
-    res.render('index');
+    const host =  `${HOST}:${PORT}`;
+    res.render('index', {host: host});
 })
 
 app.listen(PORT, HOST, () => console.log(`Server running on ${HOST}:${PORT}`));
